8e603ea4a907c4970c8661284e80ee0420418725
arthuragadzhanyan <arthur.agad@hotmail.com>
2021-04-23 22:16:03 +0300

    bless rng v6

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
+// Set up the bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -30,12 +36,6 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 };
-
-// Set up the bodyParser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 /*Develop Branch*/
 function indexCardImages() {
