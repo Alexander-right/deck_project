f0a5ab7eb402d9c23c9bbbfb13e8a7758c9c1bd0
arthuragadzhanyan <arthur.agad@hotmail.com>
2021-05-14 01:55:14 +0300

    added news route

---
+++ client/build/precache-manifest.ffc6a08be50c625f2655ea59bc5fb7ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2079a51491ab66e369f4c2fa1e91769a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa79849f3f57577ffde6",
+    "url": "/static/css/main.29612392.chunk.css"
+  },
+  {
+    "revision": "a1c2c685a7afc6db74e4",
+    "url": "/static/js/2.e5086324.chunk.js"
+  },
+  {
+    "revision": "aa79849f3f57577ffde6",
+    "url": "/static/js/main.0e09bdfb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c28abaf1baba5d7af47d1016587d9076",
+    "url": "/static/media/parchment-f5126a8249a32eb235b139078b4cc13b5fb9d2c29b0e825569312681123d721a1f2e1addb4bad78f979933e561361e2ee2c5f5881ab4fef5385c66c6276c3b44.c28abaf1.jpg"
+  },
+  {
+    "revision": "bfd62444b014b8b27a24ae30a3310a2f",
+    "url": "/static/media/wood_middle_repeat-b357e74839b5f16173c2f35d36ce22cae24d5b552e2eb1dd1fd7d3091eeccca8eb8ab5c0a2c42e9476357ac2c744e8e22ddc08c5d27be8c670fb9630065a9c01.bfd62444.png"
+  }
+]);
